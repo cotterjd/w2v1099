294e226897522a7862b5c57623badac3c70035f5
Jordan Cotter <jrdn.cttr@gmail.com>
2020-01-05 01:48:53 -0600

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <label>Spouse Salary 
+      <input v-model="spouseSalary" type="text" />
+    </label><br>
+    <label># Children 
+      <input v-model="numChildren" type="text" />
+    </label><br>
+    <div class="container">
+      <w2-taxes :numChildren="numberOfChildren" :spouseIncome="spouseIncome" />
+      <ten99-taxes :numChildren="numberOfChildren" :spouseIncome="spouseIncome" />
+      <corp-taxes :numChildren="numberOfChildren" :spouseIncome="spouseIncome" />
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import w2Taxes from './W2.vue'
+import ten99Taxes from './Ten99.vue'
+import corpTaxes from './Corp.vue'
+const { log } = console
+
+
+export default {
+  name: 'w2vs1099vscorp',
+  components: {
+    w2Taxes,
+    ten99Taxes,
+    corpTaxes,
+  },
+  data() {
+    return {
+      spouseSalary: 0,
+      numChildren: 0,
+      corpData: {
+        revenue: 0,
+        salary: 0,
+        deductions: 0,
+      },
+      w2Taxes: null,
+      ten99Taxes: null,
+      corpTaxes: null,
+      standardDeduction: 24400,
+    }
+  },
+  props: {},
+  computed: {
+    childCredit() {
+      return this.numChildren * 2000
+    },
+    spouseIncome() {
+      return Number(this.spouseSalary)
+    },
+    numberOfChildren () {
+      return Number(this.numChildren)
+    },
+  },
+  methods: {
+    calcCorp() {
+      const personalTax = this.calcW2Taxes(this.corpData.salary)
+      const revenue = Number(this.corpData.revenue)
+      const salary = Number(this.corpData.salary)
+      const deductions = Number(this.corpData.deductions)
+      const netRevenue = revenue - deductions
+      const corpTaxableIncome = (netRevenue - salary) * .8
+      const corpIncomeTaxes = corpTaxableIncome * .21
+      this.corpTaxes = corpIncomeTaxes + personalTax + salary * 0.765
+    },
+    calcW2Taxes (salary) {
+      const income = Number(salary)
+      const w2TaxableIncome = income + this.spouseIncome - this.standardDeduction 
+      return getIncomeTaxes(w2TaxableIncome) - this.childCredit + income * 0.0765  
+    }
+  }
+}
+
+function getIncomeTaxes(i) {
+  const income = Number(i)
+  const l1 = 9700;
+  const l1Taxes = l1*.1;
+  if (income < l1) {
+    return l1Taxes;
+  }
+  const l2 = 39475;
+  const l2Taxes = (l2-l1)*.12;
+  if (income < l2) {
+    return l2Taxes;
+  }
+  const l3 = 84200;
+  const l3Taxes = (l3-l2)*.22;
+  if (income < l3) {
+    return l3Taxes;
+  }
+  const l4 = 160725;
+  const l4Taxes = (l4-l3)*.24;
+  if (income < l4) {
+    return l4Taxes;
+  }
+  const l5 = 204100;
+  const l5Taxes = (l5-l4)*.32;
+  if (income < l5) {
+    return l5Taxes;
+  }
+  const l6 = 510300;
+  const l6Taxes = (l6-l5)*.35;
+  if (income < l6) {
+    return l6Taxes;
+  }
+  const l7 = 510301;
+  const l7Taxes = (income-l6)*.37; 
+  return l7Taxes;
+}
+function getTaxRate(i) {
+  const income = Number(i)
+  const l1 = 9700;
+  if (income < l1) {
+    return `10%`;
+  }
+  const l2 = 39475;
+  if (income < l2) {
+    return `12%`;
+  }
+  const l3 = 84200;
+  if (income < l3) {
+    return `22%`;
+  }
+  const l4 = 160725;
+  if (income < l4) {
+    return `24%`;
+  }
+  const l5 = 204100;
+  if (income < l5) {
+    return `32%`;
+  }
+  const l6 = 510300;
+  if (income < l6) {
+    return `35%`;
+  }
+  const l7 = 510301;
+  return `37%`;
+}
+</script>
+<style scoped>
+  .container {
+    display: grid;
+    grid-template: 1fr / 1fr 1fr 1fr;
+  }
+
+</style>
---
+++ src/components/Ten99.vue
@@ -0,0 +1,175 @@
+<template>
+  <div>
+   <div>
+     <h3>1099</h3>
+     <label>Salary 
+       <input v-model="salary" type="text" />
+     </label><br>
+     <label>Deductions 
+       <input v-model="deductions" type="text" />
+     </label><br>
+   </div>
+    <div> 
+      <span>Combined Income: {{combinedIncome}}</span><br>
+      <span>Standard Deduction: {{standardDeduction}}</span><br>
+      <span>Business Deductions: {{deductions}}</span><br>
+      <span>Total Deductions: {{totalDeductions}}</span><br>
+      <span>Taxable Income: {{taxableIncome}}</span><br>
+      <span>Payroll Taxes: {{payrollTaxes}}</span><br>
+      <span>Income Taxes: {{incomeTaxes}}</span><br>
+      <span>Credits: {{credits}}</span><br>
+      <span>Income Tax Owed: {{incomeTaxesOwed}}</span><br>
+      <span>Effective Tax Rate: {{effectiveTaxRate}}</span><br>
+      <font color="red">Total Taxes: {{totalTaxes}}</font><br>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+const { log } = console
+
+export default {
+  name: 'ten99',
+  data() {
+    return {
+      salary: 0,
+      deductions: 0,
+      standardDeduction: 24400,
+    }
+  },
+  props: {
+    spouseIncome: Number,
+    numChildren: Number,
+  },
+  computed: {
+    childCredit() {
+      return Number(this.numChildren) * 2000
+    },
+    totalDeductions() {
+      return this.standardDeduction + Number(this.deductions)
+    },
+    combinedIncome () {
+      return Number(this.salary) + this.spouseIncome
+    },
+    //marginalTaxRate () {
+    //  return getTaxRate(this.combinedIncome)
+    //},
+    incomeTaxes () {
+      return getIncomeTaxes(this.taxableIncome)
+    },
+    credits () {
+      return this.numChildren * 2000
+    },
+    netIncome () {
+      return Number(this.salary) - Number(this.deductions)
+    },
+    payrollTaxes () {
+      return (this.netIncome * 0.153) + (this.spouseIncome * 0.0765) 
+    },
+    taxableIncome () {
+      const ti = this.combinedIncome - this.totalDeductions
+      return ti < 0 ? 0 : ti 
+    },
+    incomeTaxesOwed () {
+      const taxOwed = this.incomeTaxes - this.credits 
+      return taxOwed < 0 ? 0 : taxOwed 
+    },
+    effectiveTaxRate () {
+      return this.totalTaxes / this.combinedIncome
+    },
+    totalTaxes () {
+      return this.incomeTaxesOwed + this.payrollTaxes 
+    },
+  },
+  methods: {}
+}
+
+function getIncomeTaxes(i) {
+  const income = Number(i)
+  let tax = 0
+  const l1Amount = 9700;
+  const l1Rate = .1
+  const l1Taxes = l1Amount*l1Rate;
+  if (income < l1Amount) {
+    return income*l1Rate;
+  } else tax += l1Taxes 
+
+  const l2Amount = 39475;
+  const l2Rate = .12
+  const l2Taxes = (l2Amount-l1Amount)*l2Rate;
+  if (income < l2Amount) {
+    return tax += (income - l1Amount) * l2Rate;
+  } else tax =+ l2Taxes
+
+  const l3Amount = 84200;
+  const l3Rate = .22
+  const l3Taxes = (l3Amount-l2Amount)*l3Rate;
+  if (income < l3Amount) {
+    return tax += (income - l2Amount) * l3Rate;
+  } else tax += l3Taxes
+  
+  const l4Amount = 160725;
+  const l4Rate = .24;
+  const l4Taxes = (l4Amount-l3Amount)*l4Rate;
+  if (income < l4Amount) {
+    return tax += (income - l3Amount) * l3Rate;
+  } else tax += l4Taxes
+
+  const l5Amount = 204100;
+  const l5Rate = .32;
+  const l5Taxes = (l5Amount-l4Amount)*l5Rate;
+  if (income < l5Amount) {
+    return tax == (income - l4Amount) * l5Rate;
+  } else tax += l5Taxes
+
+  const l6Amount = 510300;
+  const l6Rate = .35;
+  const l6Taxes = (l6Amount-l5Amount)*l6Rate;
+  if (income < l6Amount) {
+    return tax += (income - l5Amount) * l6Rate;
+  } else tax += l6Taxes
+
+  const l7Amount = 510301;
+  const l7Rate = .37;
+  const l7Taxes = (income-l6Amount)*l7Rate; 
+  return tax += l7Taxes;
+}
+
+function getTaxRate(i) {
+  const income = Number(i)
+  const l1 = 9700;
+  if (income < l1) {
+    return `10%`;
+  }
+  const l2 = 39475;
+  if (income < l2) {
+    return `12%`;
+  }
+  const l3 = 84200;
+  if (income < l3) {
+    return `22%`;
+  }
+  const l4 = 160725;
+  if (income < l4) {
+    return `24%`;
+  }
+  const l5 = 204100;
+  if (income < l5) {
+    return `32%`;
+  }
+  const l6 = 510300;
+  if (income < l6) {
+    return `35%`;
+  }
+  const l7 = 510301;
+  return `37%`;
+}
+</script>
+<style scoped>
+  .container {
+    display: grid;
+    grid-template: 1fr / 1fr 1fr 1fr;
+  }
+
+</style>
